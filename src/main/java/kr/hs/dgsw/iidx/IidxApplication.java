b2202b0ebf79b8a609e6ffc54dfb147cec10be38
gk7734 <gamekiller7734@naver.com>
2024-06-17 14:46:57 +0900

    first commit

---
+++ src/main/java/kr/hs/dgsw/iidx/IidxApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.iidx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IidxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IidxApplication.class, args);
+    }
+
+}
