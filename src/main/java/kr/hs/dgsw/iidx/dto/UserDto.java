5e1468fb872207c088d53c1481654fd7b97fbd6c
gk7734 <gamekiller7734@naver.com>
2024-06-23 01:12:35 +0900

    add to UseDto validation

--- src/main/java/kr/hs/dgsw/iidx/dto/UserDto.java
+++ src/main/java/kr/hs/dgsw/iidx/dto/UserDto.java
@@ -1,6 +1,9 @@
 package kr.hs.dgsw.iidx.dto;
 
 import jakarta.persistence.EnumType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import kr.hs.dgsw.iidx.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +16,32 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private Long id;
 
+    @NotBlank
+    @NotEmpty
     private String name;
 
+    @NotBlank
+    @NotEmpty
     private String diffculty;
 
+    @NotBlank
+    @NotEmpty
     private String rank;
 
+    @NotBlank
+    @NotEmpty
     private String gauge;
 
+    @NotBlank
+    @NotEmpty
     private String ex_score;
+
+    public UserDto(UserEntity entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.diffculty = entity.getDiffculty();
+        this.rank = entity.getRank();
+        this.gauge = entity.getGauge();
+        this.ex_score = entity.getEx_score();
+    }
 }
